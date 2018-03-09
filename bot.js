97abea9443c897733920c1f93940a4a2e3e18dc2
LateLama <36463857+LateLama@users.noreply.github.com>
2018-03-09 20:26:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const PREFIX = "!";
 const YTDL = require("ytdl-core");
 
 var bot = new Discord.Client();
+var contatoreTesoro = 0;
 var servers = {};
 function play(connection, message) {
   var server = servers[message.guild.id];
@@ -30,10 +31,70 @@ bot.on("message", function(message) {
   switch (args[0].toLowerCase()){
     
     //Gruppo segreto.
-    case "massoni":
-    case "massone":
-      message.channel.send("Say my name.");
+    case "covo":
+    case "covoh":
+      message.channel.send("Benvenuto nella **Caccia al tesoro del Covo**. Quando indovini un comando giusto, il messaggio che hai scritto verrà eliminato dalla chat, così che nessun altro lo legga. Mantieni il *segreto*. Ah, non c'é differenza tra maiuscolo e minuscolo nei comandi.");
       break;
+    case "minuscolo":
+    case "maiuscolo":
+      message.channel.send("Nice try, you baboon.");
+    
+    case "segreto":
+      message.delete();
+      if (contatoreTesoro >= 0) {
+        message.channel.send("Ultimamente una torta ci ha sterminati, ma ce n'é un'altra in particolare che è oltre ogni livello di comprensione umana.");
+        contatoreTesoro = 1;
+      };
+      break;
+      
+    case "whereiscake":
+    case "cake":
+    case "cakeboi":
+    case "cakeboy":
+      message.delete();
+      if (contatoreTesoro >= 1) {
+        message.channel.send("Onore a te. Se sei qui, sei sopravvissuto a questo figlio del demonio. Mica come il mio creatore.");
+        contatoreTesoro = 2;
+      }else{
+        message.channel.send("Comando non valido.");
+      };
+      break;
+      
+    case "nicastro":
+    case "nica":
+    case "andreanicastro":
+    case "nicastroandrea":
+      message.delete();
+      if (contatoreTesoro >= 2) {
+        message.channel.send("Nicastro, lui si che è un fio rahazzi. Quando sussurra è eccitante. ( ͡° ͜ʖ ͡°)");
+        contatoreTesoro = 3;
+      }else{
+        message.channel.send("Comando non valido.");
+      };
+      break;
+      
+    case "dota":
+    case "dota2":
+      message.delete();
+      if (contatoreTesoro >= 3) {
+        message.channel.send("A chi pensava che il mio creatore non lo avrebbe inserito negli indovinelli, dico solo **Kappa**. O forse per avervelo fatto scrivere, lui dovrebbe dire un'altra cosa.");
+        contatoreTesoro = 4;
+      }else{
+        message.channel.send("Comando non valido.");
+      };
+      break;
+      
+    case "ezclap":
+    case "easeglab":
+      message.delete();
+      if (contatoreTesoro >= 4) {
+        message.channel.send("Se solo ");
+        contatoreTesoro = 5;
+      }else{
+        message.channel.send("Comando non valido.");
+      };
+      break;
+      
     case "lauragori":
       message.guild.channels.find("id", "415856780674072576").overwritePermissions(message.author, {VIEW_CHANNEL: true, SEND_MESSAGES: true, ATTACH_FILES: true, ADD_REACTIONS: true, EMBED_LINKS: true, READ_MESSAGE_HISTORY: true});
       message.delete();
@@ -68,37 +129,6 @@ bot.on("message", function(message) {
       if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
       break;
 
-    //Easter egg su Not Alone.
-    case "covo":
-      message.channel.send("Riprendi in mano le carte Luogo dalla tua pila degli scarti (tranne IL COVO) OPPURE copia il potere della carta Luogo con il segnalino Creatura. Perdi un segnalino Volontà aggiuntivo se vieni catturato dal segnalino Creatura.");
-      break;
-    case "giungla":
-      message.channel.send("Riprendi in mano questa carta Luogo ed un altra carta Luogo a tua scelta dalla tua pila degli scarti.");
-      break;
-    case "fiume":
-    message.channel.send("Il prossimo turno gioca due carte Luogo. Prima di rivelarle scegline una da giocare e riprendi in mano l'altra.");
-      break;
-    case "spiaggia":
-      message.channel.send("Metti il segnalino Attivazione su La Spiaggia OPPURE, se già presente, rimuovilo per far avanzare il segnalino Soccorsi di uno spazio avanti (max. una volta a turno).");
-      break;
-    case "rover":
-      message.channel.send("Prendi dalla riserva una carta Luogo che non hai ed aggiungila alla tua mano.");
-      break;
-    case "palude":
-      message.channel.send("Riprendi in mano questa carta Luogo e due carte Luogo a tua scelta dalla pila degli scarti.");
-      break;
-    case "rifugio":
-      message.channel.send("Pesca due carte Sopravvivenza, scegline una da tenere e scarta l'altra.");
-      break;
-    case "relitto":
-      message.channel.send("Muovi avanti il segnalino Soccorsi di uno spazio (max. una volta per turno).");
-      break;
-    case "fonte":
-      message.channel.send("Pesca una carta Sopravvivenza OPPURE scegli un Sopravvissuto (compreso te stesso) e fagli riprendere un segnalino Volontà perso.");
-      break;
-    case "artefatto":
-      message.channel.send("Il prossimo turno gioca due carte Luogo e risolvile entrambe. Non puoi copiare il potere dell'artefatto.");
-      break;
     default:
       message.channel.send("Comando non valido.");
   }
