4feaa9c10a3a400c7fafabe1af4ff93d0c80b3cb
LateLama <36463857+LateLama@users.noreply.github.com>
2020-04-11 22:43:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
 const Discord = require("discord.js");
-const PREFIX = "!";
 const YTDL = require("ytdl-core");
+const PREFIX = "!";
 
 var bot = new Discord.Client();
 var servers = {};
-function play(connection, message) {
+function playYouTube(connection, message) {
   var server = servers[message.guild.id];
   server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
   server.queue.shift();
   server.dispatcher.on("end", function() {
-    if (server.queue[0]) play (connection, message);
+    if (server.queue[0]) playYouTube (connection, message);
     else connection.disconnect();
   });
 }
@@ -57,7 +57,7 @@ bot.on("message", function(message) {
       var server = servers[message.guild.id];
       server.queue.push(args[1]);
       if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
-        play(connection, message);
+        playYouTube(connection, message);
       });
       break;
     case "skip":
