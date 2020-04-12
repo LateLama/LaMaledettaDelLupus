8beafe2a92a672ee632f569ff54a933b3f96705b
LateLama <36463857+LateLama@users.noreply.github.com>
2020-04-12 22:30:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -100,12 +100,12 @@ function argsCheck(args, message, answer) {
 
 //Connessione al canale vocale.
 function connectToChannel(message){
-    if (!message.guild.voiceConnection) return message.member.voice.channel.join();
+    if (message.guild.me.voiceChannel != message.member.voiceChannel) return message.member.voice.channel.join();
 }
 
 //Disconnessione del canale vocale.
 function disconnectFromChannel(message){
-    if (message.guild.voiceConnection.voiceStatus === 0) message.member.voice.channel.leave();
+    if (message.guild.me.voiceChannel != undefined) message.member.voice.channel.leave();
 }
 
 //Riproduzione dell'audio dei video di Youtube.
