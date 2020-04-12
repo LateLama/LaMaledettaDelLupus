48e3390486764a6e3860f6d76f277856b8200afb
LateLama <36463857+LateLama@users.noreply.github.com>
2020-04-12 19:28:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -100,12 +100,12 @@ function argsCheck(args, message, answer) {
 
 //Connessione al canale vocale.
 function connectToChannel(message){
-    if (!message.guild.voiceConnection) message.member.voice.channel.join();
+    message.member.voice.channel.join();
 }
 
 //Disconnessione del canale vocale.
 function disconnectFromChannel(message){
-    if (message.guild.voice.connection) message.member.voice.channel.leave();
+    message.member.voice.channel.leave();
 }
 
 //Riproduzione dell'audio dei video di Youtube.
